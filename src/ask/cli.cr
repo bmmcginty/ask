fd4cb6dd482e5ab67d5d6ba4d3b889427561c548
Brandon McGinty <git@bmcginty.us>
2025-01-26 02:25:08 -0500

    cli: symlink answer to the latest response for easy access

--- src/ask/cli.cr
+++ src/ask/cli.cr
@@ -52,5 +52,12 @@ module Ask
     response = m.send conversation
     File.rename "question", question_filename
     File.write answer_filename, response
+    if File.exists?("answer") && !File.symlink?("answer")
+      File.delete "answer"
+    end
+    if !File.exists?("answer")
+      File.symlink answer_filename, "answer"
+    end
   end # def
-end   # module
+
+end # module
